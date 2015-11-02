49775ab01d9329b7b4499561780e195ccbc7e22c
Naturalsoul <raul.d.carrasco@outlook.com>
2015-11-02 18:16:49 -0300

    Init Commit. Happy Coding!

---
+++ LineaAzulReservas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LineaAzulReservas.Startup))]
+namespace LineaAzulReservas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
